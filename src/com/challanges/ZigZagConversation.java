20e62a05af87b627fb6c0064098c883c7ed915fd
Marius Siminic <mariussiminic@192-168-0-229.rdsnet.ro>
2021-11-30 17:39:48 +0200

    ZigZagConversion challenge.

---
+++ src/com/challanges/ZigZagConversation.java
@@ -0,0 +1,12 @@
+package com.challanges;
+
+public class ZigZagConversation {
+
+    public static void main(String[] args) {
+        System.out.println(convert("PAYPALISHIRING", 3));
+    }
+
+    public static String convert(String s, int numRows) {
+        return null;
+    }
+}
